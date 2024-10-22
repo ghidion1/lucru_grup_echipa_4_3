0242097809c0de682f9042d8d34db13e6b66eb8a
qwevsan <balancik2020.ok@gmail.com>
2024-10-22 13:08:29 +0300

    Recreated all CRUD to  comply with db

--- app/UserRepository.php
+++ app/UserRepository.php
@@ -3,18 +3,21 @@
 class UserRepository implements UserRepositoryInterface
 {
     private $pdo;
+    private $table_name;
 
     public function __construct(DatabaseConnectionInterface $databaseConnection)
     {
         $this->pdo = $databaseConnection->connect();
+        $table_name = 'angajati_db';
     }
 
     // Creare utilizator
-    public function createUser($name, $email)
+    public function createUser($nume, $prenume, $pozitie, $departament, $data_angajarii, $salariu)
     {
-        $sql = 'INSERT INTO users (name, email) VALUES (:name, :email)';
+        $sql = 'INSERT INTO :table_name (nume, prenume, pozitie, departament, data_angajarii, salariu) VALUES (:nume, :prenume, :pozitie, :departament, :data_angajarii, :salariu)';
         $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([':name' => $name, ':email' => $email]);
+        //$stmt->execute([':table_name' => $table_name, ':name' => $name, ':email' => $email]);
+        $stmt->execute([ ':table_name' => $table_name,':nume' => $nume,  ':prenume' => $prenume,  ':pozitie' => $pozitie,  ':departament' => $departament,  ':data_angajarii' => $data_angajarii,  ':salariu' => $salariu]);
         $_SESSION['message'] = "Utilizatorul a fost adăugat cu succes!";
     }
 
@@ -29,27 +32,27 @@ class UserRepository implements UserRepositoryInterface
     // Citire utilizator dupa ID
     public function getById($id)
     {
-        $sql = 'SELECT * FROM users WHERE id = :id';
+        $sql = 'SELECT * FROM :table_name WHERE id = :id';
         $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([':id' => $id]);
+        $stmt->execute([ ':table_name' => $table_name, ':id' => $id]);
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
     
     // Actualizare utilizator
     public function updateUser($id, $name, $email)
     {
-        $sql = 'UPDATE users SET name = :name, email = :email WHERE id = :id';
+        $sql = 'UPDATE :table_name SET name = :name, email = :email WHERE id = :id';
         $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([':name' => $name, ':email' => $email, ':id' => $id]);
+        $stmt->execute([':table_name' => $table_name, ':name' => $name, ':email' => $email, ':id' => $id]);
         $_SESSION['message'] = "Utilizatorul a fost actualizat cu succes!";
     }
 
     // Ștergere utilizator
     public function deleteUser($id)
     {
-        $sql = 'DELETE FROM users WHERE id = :id';
+        $sql = 'DELETE FROM :table_name WHERE id = :id';
         $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([':id' => $id]);
+        $stmt->execute([':table_name' => $table_name, ':id' => $id]);
         $_SESSION['message'] = "Utilizatorul a fost șters cu succes!";
     }
 }
